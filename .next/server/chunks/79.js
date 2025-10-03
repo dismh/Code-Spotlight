3184e2e0dbcabf6c35fd0032338e149f1af07ef3
Asctoz <likewisextra@gmail.com>
2025-10-03 15:32:11 +0530

    revert

---
+++ .next/server/chunks/79.js
@@ -0,0 +1,228 @@
+exports.id=79,exports.ids=[79],exports.modules={5673:(a,b,c)=>{"use strict";c.d(b,{cn:()=>f});var d=c(43249),e=c(58829);function f(...a){return(0,e.QP)((0,d.$)(a))}},12092:(a,b,c)=>{"use strict";c.d(b,{default:()=>d});let d=(0,c(97954).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\Abhay\\\\Downloads\\\\Kidhub\\\\Code-Spotlight\\\\src\\\\components\\\\Loader.jsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\Abhay\\Downloads\\Kidhub\\Code-Spotlight\\src\\components\\Loader.jsx","default")},17114:(a,b,c)=>{"use strict";c.d(b,{default:()=>g});var d=c(21124);c(38301);var e=c(4766);let f=e.Ay.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px; /* Space between loader and text */
+
+  .loader {
+    width: 112px;
+    height: 112px;
+  }
+
+  .box1,
+  .box2,
+  .box3 {
+    border: 16px solid white;
+    box-sizing: border-box;
+    position: absolute;
+    display: block;
+  }
+
+  .box1 {
+    width: 112px;
+    height: 48px;
+    margin-top: 64px;
+    margin-left: 0px;
+    animation: abox1 5s 0s forwards ease-in-out infinite;
+  }
+
+  .box2 {
+    width: 48px;
+    height: 48px;
+    margin-top: 0px;
+    margin-left: 0px;
+    animation: abox2 5s 0s forwards ease-in-out infinite;
+  }
+
+  .box3 {
+    width: 48px;
+    height: 48px;
+    margin-top: 0px;
+    margin-left: 64px;
+    animation: abox3 5s 0s forwards ease-in-out infinite;
+  }
+
+  .loader-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px; /* Space between the two text lines */
+    color: #ffffff; /* White text for contrast against bg-gray-900 */
+    font-family: sans-serif;
+    font-size: 18px; /* Equivalent to text-lg */
+    font-weight: 400; /* Regular weight for clean look */
+    text-align: center;
+  }
+
+  @keyframes abox1 {
+    0% {
+      width: 112px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    12.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    25% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    37.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    50% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    62.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+    75% {
+      width: 48px;
+      height: 112px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    87.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    100% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+  }
+
+  @keyframes abox2 {
+    0% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    12.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    25% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    37.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    50% {
+      width: 112px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 0px;
+    }
+    62.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    75% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    87.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    100% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+  }
+
+  @keyframes abox3 {
+    0% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    12.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    25% {
+      width: 48px;
+      height: 112px;
+      margin-top: 0px;
+      margin-left: 64px;
+    }
+    37.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 64px;
+    }
+    50% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 64px;
+    }
+    62.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 64px;
+    }
+    75% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 64px;
+    }
+    87.5% {
+      width: 48px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 64px;
+    }
+    100% {
+      width: 112px;
+      height: 48px;
+      margin-top: 64px;
+      margin-left: 0px;
+    }
+  }
+`,g=()=>(0,d.jsxs)(f,{children:[(0,d.jsxs)("div",{className:"loader",children:[(0,d.jsx)("div",{className:"box1"}),(0,d.jsx)("div",{className:"box2"}),(0,d.jsx)("div",{className:"box3"})]}),(0,d.jsxs)("div",{className:"loader-text",children:[(0,d.jsx)("p",{children:"Loading Creator Webpage..."}),(0,d.jsx)("p",{children:"Made by Dexibility"})]})]})},23100:(a,b,c)=>{Promise.resolve().then(c.bind(c,49553))},27704:(a,b,c)=>{Promise.resolve().then(c.bind(c,12092))},34359:(a,b,c)=>{"use strict";c.d(b,{default:()=>d});let d=(0,c(97954).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\Abhay\\\\Downloads\\\\Kidhub\\\\Code-Spotlight\\\\src\\\\components\\\\Navbar.jsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\Abhay\\Downloads\\Kidhub\\Code-Spotlight\\src\\components\\Navbar.jsx","default")},37432:(a,b,c)=>{Promise.resolve().then(c.bind(c,17114))},49553:(a,b,c)=>{"use strict";c.d(b,{default:()=>u});var d=c(21124),e=c(38301),f=c(5673),g=c(17207),h=c(85607),i=c(32583),j=c(85586),k=c(8220),l=c(71196);let m=({items:a,desktopClassName:b,mobileClassName:c})=>(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(o,{items:a,className:b}),(0,d.jsx)(n,{items:a,className:c})]}),n=({items:a,className:b})=>{let[c,j]=(0,e.useState)(!1);return(0,d.jsxs)("div",{className:(0,f.cn)("relative block md:hidden",b),children:[(0,d.jsx)(h.N,{children:c&&(0,d.jsx)(i.P.div,{layoutId:"nav",className:"absolute inset-x-0 bottom-full mb-2 flex flex-col gap-2",children:a.map((b,c)=>(0,d.jsx)(i.P.div,{initial:{opacity:0,y:10},animate:{opacity:1,y:0},exit:{opacity:0,y:10,transition:{delay:.05*c}},transition:{delay:(a.length-1-c)*.05},children:(0,d.jsx)("a",{href:b.href,className:"flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-900",children:(0,d.jsx)("div",{className:"h-4 w-4",children:b.icon})},b.title)},b.title))})}),(0,d.jsx)("button",{onClick:()=>j(!c),className:"flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-800",children:(0,d.jsx)(g.A,{className:"h-5 w-5 text-neutral-500 dark:text-neutral-400"})})]})},o=({items:a,className:b})=>{let c=(0,j.d)(1/0);return(0,d.jsx)(i.P.div,{onMouseMove:a=>c.set(a.pageX),onMouseLeave:()=>c.set(1/0),className:(0,f.cn)("mx-auto hidden h-16 items-end gap-4 rounded-2xl bg-gray-50 px-4 pb-3 md:flex dark:bg-neutral-900",b),children:a.map(a=>(0,d.jsx)(p,{mouseX:c,...a},a.title))})};function p({mouseX:a,title:b,icon:c,href:f}){let g=(0,e.useRef)(null),j=(0,k.G)(a,a=>{let b=g.current?.getBoundingClientRect()??{x:0,width:0};return a-b.x-b.width/2}),m=(0,k.G)(j,[-150,0,150],[40,80,40]),n=(0,k.G)(j,[-150,0,150],[40,80,40]),o=(0,k.G)(j,[-150,0,150],[20,40,20]),p=(0,k.G)(j,[-150,0,150],[20,40,20]),q=(0,l.z)(m,{mass:.1,stiffness:150,damping:12}),r=(0,l.z)(n,{mass:.1,stiffness:150,damping:12}),s=(0,l.z)(o,{mass:.1,stiffness:150,damping:12}),t=(0,l.z)(p,{mass:.1,stiffness:150,damping:12}),[u,v]=(0,e.useState)(!1);return(0,d.jsx)("a",{href:f,children:(0,d.jsxs)(i.P.div,{ref:g,style:{width:q,height:r},onMouseEnter:()=>v(!0),onMouseLeave:()=>v(!1),className:"relative flex aspect-square items-center justify-center rounded-full bg-gray-200 dark:bg-neutral-800",children:[(0,d.jsx)(h.N,{children:u&&(0,d.jsx)(i.P.div,{initial:{opacity:0,y:10,x:"-50%"},animate:{opacity:1,y:0,x:"-50%"},exit:{opacity:0,y:2,x:"-50%"},className:"absolute -top-8 left-1/2 w-fit rounded-md border border-gray-200 bg-gray-100 px-2 py-0.5 text-xs whitespace-pre text-neutral-700 dark:border-neutral-900 dark:bg-neutral-800 dark:text-white",children:b})}),(0,d.jsx)(i.P.div,{style:{width:s,height:t},className:"flex items-center justify-center",children:c})]})})}var q=c(50271),r=c(74596),s=c(1037),t=c(65945);function u(){let a=[{title:"Home",icon:(0,d.jsx)(q.A,{className:"h-full w-full text-neutral-500 dark:text-neutral-300"}),href:"/"},{title:"Websites",icon:(0,d.jsx)(r.A,{className:"h-full w-full text-neutral-500 dark:text-neutral-300"}),href:"#Websites"},{title:"Random Website!",icon:(0,d.jsx)("img",{src:"https://assets.aceternity.com/logo-dark.png",width:20,height:20,alt:"Aceternity Logo"}),href:"#"},{title:"Contributors",icon:(0,d.jsx)(s.A,{className:"h-full w-full text-neutral-500 dark:text-neutral-300"}),href:"#Contributors"},{title:"Credits",icon:(0,d.jsx)(t.A,{className:"h-full w-full text-neutral-500 dark:text-neutral-300"}),href:"#Footer"}];return(0,d.jsx)("div",{className:"flex items-center justify-center w-full fixed z-[10000]",style:{top:"50px"},children:(0,d.jsx)(m,{mobileClassName:"translate-y-20",items:a})})}},57363:(a,b,c)=>{Promise.resolve().then(c.t.bind(c,81170,23)),Promise.resolve().then(c.t.bind(c,23597,23)),Promise.resolve().then(c.t.bind(c,36893,23)),Promise.resolve().then(c.t.bind(c,89748,23)),Promise.resolve().then(c.t.bind(c,6060,23)),Promise.resolve().then(c.t.bind(c,7184,23)),Promise.resolve().then(c.t.bind(c,69576,23)),Promise.resolve().then(c.t.bind(c,73041,23)),Promise.resolve().then(c.t.bind(c,51384,23))},61135:()=>{},64956:(a,b,c)=>{Promise.resolve().then(c.bind(c,34359))},93550:(a,b,c)=>{"use strict";c.r(b),c.d(b,{default:()=>f});var d=c(75338),e=c(12092);function f(){return(0,d.jsx)(e.default,{})}},94315:(a,b,c)=>{Promise.resolve().then(c.t.bind(c,54160,23)),Promise.resolve().then(c.t.bind(c,31603,23)),Promise.resolve().then(c.t.bind(c,68495,23)),Promise.resolve().then(c.t.bind(c,75170,23)),Promise.resolve().then(c.t.bind(c,77526,23)),Promise.resolve().then(c.t.bind(c,78922,23)),Promise.resolve().then(c.t.bind(c,29234,23)),Promise.resolve().then(c.t.bind(c,12263,23)),Promise.resolve().then(c.bind(c,82146))},97634:(a,b,c)=>{"use strict";c.r(b),c.d(b,{default:()=>k,metadata:()=>j});var d=c(75338),e=c(34603),f=c.n(e),g=c(43387),h=c.n(g);c(61135);var i=c(34359);let j={title:"KidHub",description:"Kidhub is a website for Doon International School kids to display their work in a single webpage."};function k({children:a}){return(0,d.jsx)("html",{lang:"en",children:(0,d.jsxs)("body",{className:`${f().variable} ${h().variable} antialiased`,children:[(0,d.jsx)(i.default,{}),a]})})}}};
